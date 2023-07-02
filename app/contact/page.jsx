68aa3c89fa7aa9d258921d30031c321668a13803
Spiro <spiroheqimi90@gmail.com>
2023-07-02 19:11:44 +0200

    commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,10 +4,10 @@ import ContactForm from "./ContactForm"
 export default function page() {
   return (
     <div className="top-0 w-screen h-screen bg-background-color">
-      <div className="w-full h-full flex items-center justify-between gap-10 px-28 ">
-        <div className="w-full flex flex-col justify-center items-start text-mytext-color space-y-10 px-20">
-          <p className="md:text-6xl"> Get in Touch </p>
-          <p className="md:text-2xl"> spiroheqimi92@gmail.com </p>
+      <div className="w-full h-full md:flex items-center justify-between overflow-auto gap-10 md:px-28 px-10 py-10">
+        <div className="w-full sm:h-full flex flex-col justify-center items-start text-mytext-color space-y-10 px-20 ">
+          <p className="md:text-6xl text-2xl"> Get in Touch </p>
+          <p className="md:text-2xl text-xl"> spiroheqimi92@gmail.com </p>
           <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, ullam tempora adipisci illo accusantium minima molestiae, perferendis asperiores architecto ad eaque consequatur, voluptatem dolorem unde dolores ut nisi culpa iste.</p>
           <div className="z-0 flex items-start gap-5">
               
@@ -26,7 +26,7 @@ export default function page() {
             </div>
         </div>
 
-        <div className="flex flex-col w-full pt-20">
+        <div className="w-full py-10 pt-20">
           <ContactForm/>
         </div>
 
